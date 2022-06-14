6e2f9a8185cdf7f842cc9ccd935dbb3541a9c54d
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-14 16:30:20 +0300

    Добавьте файлы проекта.

---
+++ СS7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace СS7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleLine;
+            int doctorSession = 10;
+            int waitingTime;
+            int hour = 60;
+            int min = 60;
+            Console.Write("Введите кол-во людей в очереди: ");
+            peopleLine = Convert.ToInt32(Console.ReadLine());
+            waitingTime = peopleLine * doctorSession;
+            hour = waitingTime / hour;
+            min = waitingTime % min;
+            Console.WriteLine("Вы должны отстоять в очереди: " + hour + " часа и " + min + " мин");
+        }
+    }
+}
